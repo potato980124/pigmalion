660089400f7628a512ed694f21ba5d27da63316d
potato990124 <potato980124@gmail.com>
2022-12-13 13:54:19 +0900

    메인 작업 ing

--- fatsecreat/js/fatscreat.js
+++ fatsecreat/js/fatscreat.js
@@ -1,3 +1,50 @@
+// 헤더
+let gnb = document.querySelectorAll('.gnb_menu');
+let lnb = document.querySelectorAll('.lnb');
+let gnbBg = document.querySelector('#gnb_bg');
+let innerHeader = document.querySelector('.inner_header');
+let gnbUnderline = document.querySelectorAll('.g_under_line');
+
+gnb.forEach((e,index)=>{
+  e.addEventListener('mouseenter',()=>{
+    setTimeout(()=>{
+      lnb[index].style.display = 'flex';
+      gnbUnderline[index].style.width = '100%';
+      gnbBg.style.height = '170px';
+      e.firstElementChild.style.color = '#91CF00';
+    },300);
+    setTimeout(()=>{
+      lnb[index].style.opacity = '1';
+    },500)
+  });
+    e.addEventListener('mouseleave',()=>{
+      setTimeout(()=>{
+          lnb[index].style.display = 'none';
+          gnbUnderline[index].style.width = '0';
+          gnbBg.style.height = '0';
+          e.firstElementChild.style.color = '#ddd';
+      },300);
+      setTimeout(()=>{
+        lnb[index].style.opacity = '0';
+      },500)
+    })
+})
+
+
+/* 모바일 메뉴박스 아코디언  */
+
+let plusBtn = document.querySelector('.m_menu_box_wrap');
+let lotateMinus = document.querySelector('#m_menu_bar_btn2');
+let mCont = document.querySelector('.m_menu_cont');
+
+plusBtn.addEventListener('click',()=>{
+  lotateMinus.classList.toggle('m_menu_bar_btn_click');
+  mCont.classList.toggle('m_menu_cont_click');
+})
+
+
+
+// 비만도 계산기
 let height = document.querySelector('#height');
 let weight = document.querySelector('#weight');
 let age = document.querySelector('#age');
@@ -27,40 +74,7 @@ function onCalc(e) {
   e.preventDefault(); // 안 써 주면 자동으로 다음 인풋값이 들어간다 다음 인풋값은 입력이 안 되었기 때문에 새로고침이 된다
 }
 
-let gnb = document.querySelectorAll('.gnb_menu');
-let lnb = document.querySelectorAll('.lnb');
-let gnbBg = document.querySelector('#gnb_bg');
-let innerHeader = document.querySelector('.inner_header');
-let gnbUnderline = document.querySelectorAll('.g_under_line');
 
-gnb.forEach((e,index)=>{
-  e.addEventListener('mouseenter',()=>{
-    setTimeout(()=>{
-      lnb[index].style.display = 'flex';
-      gnbUnderline[index].style.width = '100%';
-      gnbBg.style.height = '170px';
-    },300);
-    setTimeout(()=>{
-      lnb[index].style.opacity = '1';
-    },500)
-  });
-    e.addEventListener('mouseleave',()=>{
-      setTimeout(()=>{
-          lnb[index].style.display = 'none';
-          gnbUnderline[index].style.width = '0';
-          gnbBg.style.height = '0';
-      },300);
-      setTimeout(()=>{
-        lnb[index].style.opacity = '0';
-      },500)
-    })
-})
-
-// lnb.forEach((e,index)=>{
-//   e.addEventListener('mouseenter',()=>{
-//     lnb[index].style.display = 'flex';
-//   })
-// })
 
 
 
