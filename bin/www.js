bfef8e8f4d2f41ecfdb5355805a627f5004b9f5f
bumjin <potato980124@gmail.com>
2023-05-18 15:36:46 +0900

    포트변경

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,7 @@
 let app = require('../app');
-let PORT = 5900;
+let PORT = process.env.PORT || 5900;
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
   console.log(`${PORT}로 express 실행`);
   console.log(`http://localhost:${PORT}`);
 })
