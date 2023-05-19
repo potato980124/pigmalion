ed03e4a1e6bd2b4c4465949980c1219958c1b899
bumjin <potato980124@gmail.com>
2023-05-19 11:57:18 +0900

    환경변수 이름변경

--- routes/route.js
+++ routes/route.js
@@ -14,7 +14,7 @@ const request = require("request");
 // 세션 (미들웨어) 6
 router.use(
   session({
-    secret: process.env.blackzat, // 데이터를 암호화 하기 위해 필요한 옵션
+    secret: process.env.COOKIE_SECRET || 'blackzat', // 데이터를 암호화 하기 위해 필요한 옵션
     resave: false, // 요청이 왔을때 세션을 수정하지 않더라도 다시 저장소에 저장되도록
     saveUninitialized: false, // 세션이 필요하면 세션을 실행시칸다(서버에 부담을 줄이기 위해)
     store: new FileStore(), // 세션이 데이터를 저장하는 곳
