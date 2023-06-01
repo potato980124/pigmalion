1884098b7fba6b37cbc80a30881eb8e0ac22f880
bumjin <potato980124@gmail.com>
2023-06-01 17:56:36 +0900

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 const expressLayout = require('express-ejs-layouts');
 const config = {
   host: 'database-1.ckqtywedccws.ap-northeast-1.rds.amazonaws.com',
-  port: 3306,
+  port: process.env.DB_PORT || 3306,
   user: 'admin',
   password: 'ghdqjawls12',
   database: 'pigmalion',
@@ -24,6 +24,8 @@ app.use(
     store: new MySQLStore(config),
   })
 );
+app.use(express.static(path.join(__dirname, 'public'))); // 익스프레스 안의 스태틱을 사용하는거다 그냥 스태틱이 아님, 얘는 경로를 일일히 세팅 하게 않하려고 써주는것 여기서 경로를 지정 해주는것
+//스태틱은 정적인 애들 css html을 읽어주는 것 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -31,7 +33,6 @@ app.set('views', path.join(__dirname, 'views')); //ejs 쓸 때 쓰면 된다.
 app.set('view engine', 'ejs'); //ejs 쓸 때 쓰면 된다.
 app.use(expressLayout);
 app.use('/', routers);
-app.use(express.static(path.join(__dirname, 'public'))); // 익스프레스 안의 스태틱을 사용하는거다 그냥 스태틱이 아님, 얘는 경로를 일일히 세팅 하게 않하려고 써주는것 여기서 경로를 지정 해주는것
-//스태틱은 정적인 애들 css html을 읽어주는 것 
+
 
 module.exports = app; //app이라는 이름으로 묘둘화 시켜서 내보내겠다.
