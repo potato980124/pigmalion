557e8f88a0db5d3d597dad61282efdee163006bd
bumjin <potato980124@gmail.com>
2023-06-02 16:13:04 +0900

    test

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ const MySQLStore = require("express-mysql-session")(session);
 const FileStore = require("session-file-store")(session); // 세션을 파일에 저장
 const bodyParser = require('body-parser');
 const expressLayout = require('express-ejs-layouts');
-const config = {
-  host: 'database-1.ckqtywedccws.ap-northeast-1.rds.amazonaws.com',
-  port: process.env.DB_PORT || 3306,
-  user: 'admin',
-  password: 'ghdqjawls12',
-  database: 'pigmalion',
-};
+// const config = {
+//   host: 'database-1.ckqtywedccws.ap-northeast-1.rds.amazonaws.com',
+//   port: process.env.DB_PORT || 3306,
+//   user: 'admin',
+//   password: 'ghdqjawls12',
+//   database: 'pigmalion',
+// };
 app.use(
   session({
     secret: 'blackzat', // 데이터를 암호화 하기 위해 필요한 옵션
     resave: false, // 요청이 왔을때 세션을 수정하지 않더라도 다시 저장소에 저장되도록
     saveUninitialized: true, // 세션이 필요하면 세션을 실행시칸다(서버에 부담을 줄이기 위해)
-    store: new MySQLStore(config),
-    secure: true,
-    httpOnly: true, 
-    // store: new FileStore(),
+    // store: new MySQLStore(config),
+    // secure: true,
+    // httpOnly: true, 
+    store: new FileStore(),
   })
 );
 app.use(express.static(path.join(__dirname, 'public'))); // 익스프레스 안의 스태틱을 사용하는거다 그냥 스태틱이 아님, 얘는 경로를 일일히 세팅 하게 않하려고 써주는것 여기서 경로를 지정 해주는것
