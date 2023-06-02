5465e41154f36ba40c2f28692832d18241b0392a
bumjin <potato980124@gmail.com>
2023-06-02 20:50:47 +0900

    test

--- app.js
+++ app.js
@@ -5,23 +5,17 @@ const routers = require('./routes/route'); //라우트스 파일에있는 라우
 const app = express();
 const path = require('path');
 const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
+// const MySQLStore = require("express-mysql-session")(session);
 const FileStore = require("session-file-store")(session); // 세션을 파일에 저장
 const bodyParser = require('body-parser');
 const expressLayout = require('express-ejs-layouts');
-const config = {
-  host: 'database-1.ckqtywedccws.ap-northeast-1.rds.amazonaws.com',
-  port: process.env.DB_PORT || 3306,
-  user: 'admin',
-  password: 'ghdqjawls12',
-  database: 'pigmalion',
-};
+
 app.use(
   session({
     secret: 'blackzat', // 데이터를 암호화 하기 위해 필요한 옵션
     resave: false, // 요청이 왔을때 세션을 수정하지 않더라도 다시 저장소에 저장되도록
     saveUninitialized: true, // 세션이 필요하면 세션을 실행시칸다(서버에 부담을 줄이기 위해)
-    store: new MySQLStore(config),
+    store: new FileStore(),
     cookie: {
       maxAge: 24 * 60 * 60 * 1000, // 24시간
       httpOnly: true,
