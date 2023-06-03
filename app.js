cf27f8dd1372fde80306c2ba61301a727b0f7106
bumjin <potato980124@gmail.com>
2023-06-03 16:40:30 +0900

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routers = require('./routes/route'); //라우트스 파일에있는 라우
 const app = express();
 const path = require('path');
 const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
+// const MySQLStore = require("express-mysql-session")(session);
 const FileStore = require("session-file-store")(session); // 세션을 파일에 저장
 const bodyParser = require('body-parser');
 const expressLayout = require('express-ejs-layouts');
@@ -22,7 +22,6 @@ app.use(
 app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views')); //ejs 쓸 때 쓰면 된다.
 app.set('view engine', 'ejs'); //ejs 쓸 때 쓰면 된다.
-// app.use(bodyParser.json());
 app.use(expressLayout);
 app.use('/', routers);
 app.use(express.static(path.join(__dirname, 'public'))); // 익스프레스 안의 스태틱을 사용하는거다 그냥 스태틱이 아님, 얘는 경로를 일일히 세팅 하게 않하려고 써주는것 여기서 경로를 지정 해주는것
