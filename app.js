cbcdcd451370ecf6470b90e6ab620ccd192773ae
bumjin <potato980124@gmail.com>
2023-06-03 16:32:35 +0900

    test

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ app.use(
     store: new FileStore(),
   })
 );
-app.use(bodyParser.json());
 app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views')); //ejs 쓸 때 쓰면 된다.
 app.set('view engine', 'ejs'); //ejs 쓸 때 쓰면 된다.
+// app.use(bodyParser.json());
 app.use(expressLayout);
 app.use('/', routers);
 app.use(express.static(path.join(__dirname, 'public'))); // 익스프레스 안의 스태틱을 사용하는거다 그냥 스태틱이 아님, 얘는 경로를 일일히 세팅 하게 않하려고 써주는것 여기서 경로를 지정 해주는것
